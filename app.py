829f8498d33b21be81880c2321eef3219577938e
108charlotte <152224795+108charlotte@users.noreply.github.com>
2025-02-18 21:36:59 +0000

    added dream text

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/welcome')
+def welcome(): 
+    return "It works!"
+
+@app.route('/static/<path:filename>')
+def static_files(filename):
+    return send_from_directory('static', filename)
+
 if __name__ == '__main__':
     app.run(debug=True)
